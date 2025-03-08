18c31dc8b505305e68becf49c9ef949c84a04250
Vasquez <vasquez0215@outlook.com>
2025-03-07 23:04:06 -0600

    Agregar archivos de proyecto.

---
+++ Practica20250307.AppMVCCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20250307.AppMVCCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Practica20250307.AppMVCDataFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20250307.AppMVCDataFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
