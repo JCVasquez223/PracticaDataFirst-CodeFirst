7c22ff25831ed6950525fee5c5136577db71b7f6
Vasquez <vasquez0215@outlook.com>
2025-03-08 12:37:33 -0600

    Se realizo la migracion para generar los modelos de la base de datos e configuro el DbContext.

---
+++ Practica20250307.AppMVCDataFirst/Models/CatalogosDbContext.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practica20250307.AppMVCDataFirst.Models;
+
+public partial class CatalogosDbContext : DbContext
+{
+    public CatalogosDbContext()
+    {
+    }
+
+    public CatalogosDbContext(DbContextOptions<CatalogosDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    public virtual DbSet<Equipo> Equipos { get; set; }
+
+    public virtual DbSet<Estado> Estados { get; set; }
+
+    public virtual DbSet<MetodosDePago> MetodosDePagos { get; set; }
+
+    public virtual DbSet<Moneda> Monedas { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Proveedore> Proveedores { get; set; }
+
+    public virtual DbSet<Proyecto> Proyectos { get; set; }
+
+    public virtual DbSet<Servicio> Servicios { get; set; }
+
+    public virtual DbSet<TiposDeDocumento> TiposDeDocumentos { get; set; }
+
+    public virtual DbSet<UnidadesDeMedidum> UnidadesDeMedida { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC075CCD12BC");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC07E13990AC");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departam__3214EC0793871BA1");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Empleado__3214EC07A66475EC");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Puesto).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Equipo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Equipos__3214EC07C4F07BFC");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Estado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Estados__3214EC0727EC10B4");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<MetodosDePago>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__MetodosD__3214EC077796EDCF");
+
+            entity.ToTable("MetodosDePago");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Moneda>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Monedas__3214EC078427EEB4");
+
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+            entity.Property(e => e.Simbolo).HasMaxLength(10);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC071E252A15");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+        });
+
+        modelBuilder.Entity<Proveedore>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Proveedo__3214EC07B4317D9E");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Proyecto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Proyecto__3214EC0758BFFC6E");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Servicio>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Servicio__3214EC07D9AF2908");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<TiposDeDocumento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__TiposDeD__3214EC07413EC7F0");
+
+            entity.ToTable("TiposDeDocumento");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<UnidadesDeMedidum>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Unidades__3214EC07144316C8");
+
+            entity.Property(e => e.Abreviatura).HasMaxLength(10);
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
