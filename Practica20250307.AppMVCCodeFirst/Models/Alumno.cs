c164459a00e5b851b21da5387019c7c3b682f646
Vasquez <vasquez0215@outlook.com>
2025-03-09 16:46:10 -0600

    Se creo el modelo de la tabla Alumno.

---
+++ Practica20250307.AppMVCCodeFirst/Models/Alumno.cs
@@ -0,0 +1,15 @@
+﻿namespace Practica20250307.AppMVCCodeFirst.Models
+{
+    public class Alumno
+    {
+           
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+      
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public short YearEstudio { get; set; }
+    }
+}
