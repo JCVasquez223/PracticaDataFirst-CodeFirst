6a887a381427adf860147460cdc481725c26ef96
Vasquez <vasquez0215@outlook.com>
2025-03-08 22:13:28 -0600

    Se creo el DbContext asi como se configuro la conexion a la base de datos en el json.

---
+++ Practica20250307.AppMVCCodeFirst/Models/CodeFirstDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practica20250307.AppMVCCodeFirst.Models
+{
+    public class CodeFirstDbContext : DbContext
+    {
+
+        public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options) : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+
+    }
+}
